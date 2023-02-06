f22a48926973a17e31435cc8b507917b66ba4124
HansumaeCecilia <48033@edu.raseko.fi>
2023-02-06 11:17:58 +0200

    Admin-modelin luonti

--- blogi/admin.py
+++ blogi/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Postaus
+
+# Luo model-admin
+@admin.register(Postaus)
+class PostausAdmin(admin.ModelAdmin):
+    pass
