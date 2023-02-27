5763aa685a266e04d09902dfe18638830ce8a6ee
HansumaeCecilia <48033@edu.raseko.fi>
2023-02-27 13:04:52 +0200

    Kopsattu opelta

--- blogi/views.py
+++ blogi/views.py
@@ -1,23 +1,45 @@
 from django.shortcuts import redirect, render
 from django.urls import reverse
+
 from .models import Postaus
 
-# Create your views here.
-def postaukset (request):
-    postaukset = Postaus.objects.all()    
+
+def postaukset(request):
+    postaukset = Postaus.objects.all()
     context = {"postaukset": postaukset}
-    return render(request, "blogi/postauslista.html", context)
+    return render(
+        request,
+        "blogi/postauslista.html",
+        context,
+    )
+
 
 def nayta_postaus(request, id):
     postaus = Postaus.objects.get(id=id)
-    context = {'postaus': postaus}
+    context = {"postaus": postaus}
     return render(request, "blogi/postaus.html", context)
 
+
 def uusi_postaus(request):
-    if request.method == "POST":
+    if request.method == "GET":
+        # 1. Lomake näytetään ensimmäistä kertaa
+        return render(request, "blogi/uusi_postaus.html")
+
+    elif request.method == "POST":
+        # 2. Käyttäjä täytti ja lähetti lomakkeen
+
+        # Luetaan lomakkeen tiedot POST-datasta
         otsikko = request.POST['otsikko']
         teksti = request.POST['teksti']
-        postaus = Postaus.objects.create(otsikko = otsikko, teksti = teksti)
-        return redirect(reverse('nayta_postaus', args=(postaus.id)))            
-    
-    return render(request, 'blogi/uusi_postaus.html')
+
+        # Luodaan uusi Postaus-objekti tietokantaan
+        postaus = Postaus.objects.create(
+            otsikko=otsikko,
+            teksti=teksti,
+        )
+
+        # Muodostetaan URL-osoite luotuun Postaus-objektiin
+        url = reverse('nayta_postaus', args=(postaus.id,))
+
+        # Palautetaan uudelleenohjaus uuden Postaus-objektin URL:iin
+        return redirect(url)
