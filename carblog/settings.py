3da92ae47b1ed8b343beb99bde088afc9dc5c39f
HansumaeCecilia <48033@edu.raseko.fi>
2023-03-06 14:27:04 +0200

    Ei muutoksia

--- carblog/settings.py
+++ carblog/settings.py
@@ -23,7 +23,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # osoitteen (URL) alkuun
 MEDIA_URL = "/media/"
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
